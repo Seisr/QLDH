a1352d0d1beea5c1ead27340dfba6a2fd8dea000
Kha <khanguyen1204@gmail.com>
2023-10-18 13:25:39 +0100

    feat: init project

---
+++ src/qldh/QLDH.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package qldh;
+
+import view.LoginView;
+
+/**
+ *
+ * @author A715-42G
+ */
+public class QLDH {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+    
+}
